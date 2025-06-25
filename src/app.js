d46d3e1cbc54448302900f27b253f4bc9a82e997
Luana Ramirez <luanaabigail168@gmail.com>
2025-06-25 19:36:28 -0300

    inicialicé para conectarme a la base de datos

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import { routerCharacter } from "./routers/character.routes.js";
+import { startDB } from "./database/database.js";
+
 dotenv.config();
 
 const app = express();
@@ -8,6 +10,7 @@ const PORT = process.env.PORT;
 
 app.use("/api", routerCharacter);
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
+  await startDB();
   console.log("Server running in port: " + PORT);
 });
