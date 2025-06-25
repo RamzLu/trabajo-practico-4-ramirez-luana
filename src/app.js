8801398ca331146e5110958afe4357c96ce1bcc3
Luana Ramirez <luanaabigail168@gmail.com>
2025-06-25 19:11:39 -0300

    Modularicé las rutas a controllers y añadí la ruta /api en  app.js

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
-app.use(routerCharacter);
+app.use("/api", routerCharacter);
 
 app.listen(PORT, () => {
   console.log("Server running in port: " + PORT);
--- src/routers/character.routes.js
+++ src/routers/character.routes.js
@@ -1,7 +1,6 @@
 import { Router } from "express";
+import { getAll } from "../controllers/character.controllers.js";
 
 export const routerCharacter = Router();
 
-routerCharacter.get("/", (req, res) => {
-  res.send("All characters");
-});
+routerCharacter.get("/character", getAll);
