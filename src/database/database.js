05faef786dabd14ceb85d8b16f45c4368e3f42c6
Luana Ramirez <luanaabigail168@gmail.com>
2025-06-25 19:45:11 -0300

    me conecté a phpmyadmin correctamente y añadí los ejemplos de las vartiables de entorno

--- src/database/database.js
+++ src/database/database.js
@@ -1,9 +1,15 @@
 import { Sequelize } from "sequelize";
+import "dotenv/config";
 
-const sequelize = new Sequelize("database", "username", "password", {
-  host: "localhost",
-  dialect: "mysql",
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+  }
+);
 
 export const startDB = async () => {
   try {
